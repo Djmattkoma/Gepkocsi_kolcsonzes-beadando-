0e4338240cd2e2e210d7e94bcaae101a7f3ebde5
Djmattkoma <lmateka1999@gmail.com>
2020-03-03 19:04:04 +0100

    javítva

--- Application/Templates/layout.php
+++ Application/Templates/layout.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?= APPLICATION ?>Style/style.css" text="text/css">
+
     <title><?=$page?></title>
 </head>
 <body>
-<?php require_once APPLICATION.'Templates/headerView.php' ?>
-    <?php require_once APPLICATION."Templates/{$page}View.php" ?>
+	<?php require_once APPLICATION.'Templates/headerView.php' ?>
+	<?php require_once APPLICATION."Templates/{$page}View.php"?>
+
 </body>
 </html>
